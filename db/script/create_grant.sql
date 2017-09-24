b51b161ca2b917ce6e998a798a5cbec416f51b6c
TomoProg <helloworld0306.xxx@gmail.com>
2017-09-24 14:18:33 +0900

    Grantの構文を開発用環境用に変更

--- db/script/create_grant.sql
+++ db/script/create_grant.sql
@@ -2,8 +2,8 @@
 	GRANT作成用SQL
 	DB名、ユーザ名、パスワードを入れること
 */
-GRANT ALL PRIVILEGES ON `db_name`.* TO `dbuser`@localhost IDENTIFIED BY '12345678';
-GRANT ALL PRIVILEGES ON `db_name`.* TO `dbuser`@'192.168.%' IDENTIFIED BY '12345678';
-GRANT ALL PRIVILEGES ON `db_name`.* TO `dbuser`@'172.%' IDENTIFIED BY '12345678';
+GRANT ALL PRIVILEGES ON `app_development`.* TO `dbuser`@localhost IDENTIFIED BY '12345678';
+GRANT ALL PRIVILEGES ON `app_development`.* TO `dbuser`@'192.168.%' IDENTIFIED BY '12345678';
+GRANT ALL PRIVILEGES ON `app_development`.* TO `dbuser`@'172.%' IDENTIFIED BY '12345678';
 GRANT FILE ON *.* TO `dbuser`@'172.%';
 
