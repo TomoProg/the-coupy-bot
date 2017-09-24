6671ffb636b9f4c78560e865ee2efeb5b8eaec03
TomoProg <helloworld0306.xxx@gmail.com>
2017-09-24 13:37:27 +0900

    Device導入

--- web/app/config/routes.rb
+++ web/app/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  #devise_for :managers
+
+  devise_for :managers, controllers: {
+    sessions: 'managers/sessions'
+  }
   resources :keywords
   post '/callback', to: 'linebot#callback'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- web/app/db/schema.rb
+++ web/app/db/schema.rb
@@ -10,13 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915161633) do
+ActiveRecord::Schema.define(version: 20170924023444) do
 
-  create_table "keywords", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "keywords", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "send_msg"
     t.string "response_msg"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "managers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_managers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_managers_on_reset_password_token", unique: true
   end
 
 end
