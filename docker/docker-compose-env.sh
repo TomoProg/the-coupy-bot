706cf2934edc8f6b9d4d924c72415740cfd107fb
TomoProg <helloworld0306.xxx@gmail.com>
2017-09-24 09:36:33 +0900

    開発環境のDBを設定

--- docker/docker-compose-env.sh
+++ docker/docker-compose-env.sh
@@ -1,6 +1,6 @@
 export MYSQL_USER=dbuser
 export MYSQL_PASSWD=12345678
-export MYSQL_DATABASE=db_name
+export MYSQL_DATABASE=app_development
 #export APP_ENV=production
 export LINE_CHANNEL_SECRET=dummy
 export LINE_CHANNEL_TOKEN=dummy
